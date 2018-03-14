7e69f39d1a39854a3ea0ca3b23fe656361a04d88
maf-ia <mafteam.ia@gmail.com>
2018-03-14 22:48:40 +0100

    code sous forme de classe pour factoriser les parametres

--- cracking/asm.py
+++ cracking/asm.py
@@ -1,28 +1,42 @@
 
-def rol_one( val, size ):
-    return ( (val << 1) & (2**size - 1)) + ( (val >> (size-1) ) & 0x1 )    
+class asm:
+    def __init__( self, archsize ):
+        self.archsize = archsize
+        self.mask = ( 2**archsize - 1 )
+        self.mask8 = ( 2**(archsize-8) - 1 ) << 8
+        self.mask16 = ( 2**(archsize-16) - 1 ) << 16
+        self.mask32 = ( 2**(archsize-32) - 1 ) << 32
 
-def ror_one( val, size ):
-    return ( (val >> 1) & (2**size - 1)) + ( (val << (size-1)) & (2**size - 1) )
+    # ADD
+    def add8( self, dest, val ):
+        return (dest & self.mask8) + ( (dest + val) & 0xFF )
+    
+    def add16( self, dest, val ):
+        return (dest & self.mask16) + ( (dest + val) & 0xFFFF )
+    
+    def add32( self, dest, val ):
+        return (dest & self.mask32) + ( (dest + val) & 0xFFFFFFFF )
+    
+    def add64( self, dest, val ):
+        return (dest + val) & 0xFFFFFFFFFFFFFFFF
+    
+    # ROL
+    def rol_one( self, val ):
+        return ( (val << 1) & self.mask) + ( (val >> (self.archsize-1) ) & 0x1 )    
 
-def rol32( val, nb ):
-    for i in range(nb):
-        val = rol_one( val, 32 )
-    return val
+    def rol( self, val, nb ):
+        for i in range(nb):
+            val = self.rol_one( val )
+        return val
 
-def ror32( val, nb ):
-    for i in range(nb):
-        val = ror_one( val,32 )
-    return val
+    # ROR
+    def ror_one( self, val, size ):
+        return ( (val >> 1) & self.mask) + ( (val << (self.archsize-1)) & self.mask )
 
-def rol64( val, nb ):
-    for i in range(nb):
-        val = rol32one( val, 64 )
-    return val    
+    def ror( self, val, nb ):
+        for i in range(nb):
+            val = self.ror_one( val )
+        return val    
 
-def ror64( val, nb ):
-    for i in range(nb):
-        val = ror_one( val, 64 )
-    return val
 
 
