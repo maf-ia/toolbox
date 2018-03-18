fcf6850df4e69647c5152806dca478aab3c9270e
maf-ia <mafteam.ia@gmail.com>
2018-03-18 12:34:35 +0100

    amelioration des fonctions de manipulation de PNG, pas encore très clean

--- stegano/PNGReader.py
+++ stegano/PNGReader.py
@@ -1,5 +1,39 @@
 
 
+# http://www.libpng.org/pub/png/spec/1.2/PNG-Contents.html
+
+def readDWORD( data, offset ):
+    ret = 0
+    for i in range( 4 ):
+        ret *= 256
+        ret += data[i+offset]
+    return ret
+
+def readWORD( data, offset ):
+    ret = 0
+    for i in range( 2 ):
+        ret *= 256
+        ret += data[i+offset]
+    return ret
+
+def readBYTE( data, offset ):
+    return data[ofset]
+
+def writeDWORD( data, offset, val ):
+    for i in range( 4 ):
+        data[offset+i] = ( (val >> (8*(3-i))) & 0xFF )
+    return data
+
+def writeWORD( data, offset, val ):
+    for i in range( 2 ):
+        data[offset+i] = ( (val >> (8*(1-i))) & 0xFF )
+        
+    return data
+
+def writeBYTE( data, offset, val ):
+    data[offset] = ( val & 0xFF )
+    return data
+
 class PNGReader:
     def __init__( self, filename ):
         self.filename = filename
@@ -12,7 +46,7 @@ class PNGReader:
         return checksum
         
     def checkSignature( self, data ):
-        if data[:8] == b'\x89PNG\r\n\x1a\n':
+        if data[:8] == list(b'\x89PNG\r\n\x1a\n'):
             val = "Signature OK\n"
         else:
             val = "Error in signature\n"
@@ -31,7 +65,7 @@ class PNGReader:
         if chunk[1] != 'IHDR':
             val += "Wrong type for header chunk: " + chunk[1] + " found\n"
         elif not self.checkChunkCRC( chunk ):
-            val += "Bad CRC - computed " + str(self.computeChunkCRC( chunk )) + ", found " + str(chunk[3])
+            val += "Bad CRC - computed " + str() + ", found " + str(chunk[3]) + "\n"
         return val
     
     def readChunk( self, data ):
@@ -45,22 +79,40 @@ class PNGReader:
         for d in data[8+length:12+length]:
             crc *= 256
             crc += d
+        crc2 = readDWORD(data[8+length:12+length],0)
+        print("crc",crc,crc2)
         return (length, chunkType, chunkData, crc )
     
+    def buildChunk( self, tag, data ):
+        length = len(data)
+        chunkData = [0] * (length+12)
+        chunkData = writeDWORD( chunkData, 0, length )
+        for i in range(4):
+            chunkData[4+i] = ord(tag[i])
+        for i in range(length):
+            chunkData[8+i] = data[i]
+        crc = self.computeCRC( bytes(chunkData[4:-4]) )
+        chunkData = writeDWORD( chunkData, 8+length, crc ) 
+        return chunkData
     
     def displayHeader( self ):
         with open( self.filename, "rb" ) as fic:
-            data = fic.read()
+            data = list(fic.read())
         val = ""
         val += self.checkSignature( data )
         header = self.readChunk( data[8:] )
         val += self.checkHeader( header )
+        headerData = header[2]
+        w = readDWORD( headerData, 0 )
+        h = readDWORD( headerData, 4 )
+        val += "Width: " + str(w) + "\n"
+        val += "Height: " + str(h) + "\n"
         
         print( val )
     
     def readChunks( self ):
         with open( self.filename, "rb" ) as fic:
-            data = fic.read()
+            data = list(fic.read())
         idx = 8
         cpt = 1
         ret = []
@@ -85,5 +137,30 @@ class PNGReader:
                 if self.computeCRC( bytes( allData ) ) == chunk[3]:
                     print( i, j )
                     return
+                
+    def modifySize( self, width, height, filename ):
+        with open( self.filename, "rb" ) as fic:
+            data = list(fic.read())
+        header = self.readChunk( data[8:] )
+        headerData = header[2]
+        print(headerData)
+        headerData = writeDWORD( headerData, 0, width )
+        headerData = writeDWORD( headerData, 4, height )
+        print(headerData)
+        chunkData = self.buildChunk( "IHDR", headerData )
+        #dataList = list(data)
+        for i in range( len(chunkData) ):
+            data[8+i] = chunkData[i]
+        with open( filename, "wb" ) as fic:
+            fic.write(bytes(data))
         
+       
+png = PNGReader( "test.png" )
+png.displayHeader()
+for i in range(10):
+    png.modifySize( 200+i, 200, "test"+("000"+str(i))[-3:]+".png" )
+
+
+
+
         
